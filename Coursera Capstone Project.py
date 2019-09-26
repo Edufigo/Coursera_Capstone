126b6e0bcecc5680fe21044c6dced3290532f36e
Edufigo <55803750+Edufigo@users.noreply.github.com>
2019-09-26 00:46:46 -0300

    Add files via upload

---
+++ Coursera Capstone Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this Notebook will be used for the Capstone 
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
